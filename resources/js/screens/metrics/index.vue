ae7c055d2dc85d1654ffb61080b2eafa213139e0
irkali <iraklishengelia94@gmail.com>
2025-11-29 12:08:18 +0400

    fix nav pills

--- resources/js/screens/metrics/index.vue
+++ resources/js/screens/metrics/index.vue
@@ -16,15 +16,15 @@
                 <h2 class="text-base font-semibold m-0">Metrics</h2>
             </div>
 
-            <ul class="nav nav-pills card-bg-secondary">
+            <ul class="nav nav-pills card-bg-secondary flex">
                 <li class="nav-item">
-                    <router-link class="nav-link text-decoration-none" active-class="active" :to="{ name: 'metrics-jobs'}" href="#">
+                    <router-link class="nav-link no-underline" active-class="active" :to="{ name: 'metrics-jobs'}" href="#">
                         Jobs
                     </router-link>
                 </li>
 
                 <li class="nav-item">
-                    <router-link class="nav-link text-decoration-none" active-class="active" :to="{ name: 'metrics-queues'}" href="#">
+                    <router-link class="nav-link no-underline" active-class="active" :to="{ name: 'metrics-queues'}" href="#">
                         Queues
                     </router-link>
                 </li>
--- resources/js/screens/monitoring/tag.vue
+++ resources/js/screens/monitoring/tag.vue
@@ -9,15 +9,15 @@
                 <h2 class="text-base font-semibold m-0">Recent Jobs for "{{ $route.params.tag }}"</h2>
             </div>
 
-            <ul class="nav nav-pills card-bg-secondary">
+            <ul class="nav nav-pills card-bg-secondary flex">
                 <li class="nav-item">
-                    <router-link class="nav-link text-decoration-none" active-class="active" :to="{ name: 'monitoring-jobs', params: { tag:$route.params.tag }}" href="#">
+                    <router-link class="nav-link no-underline" active-class="active" :to="{ name: 'monitoring-jobs', params: { tag:$route.params.tag }}" href="#">
                         Recent Jobs
                     </router-link>
                 </li>
 
                 <li class="nav-item">
-                    <router-link class="nav-link text-decoration-none" active-class="active" :to="{ name: 'monitoring-failed', params: { tag:$route.params.tag }}" href="#">
+                    <router-link class="nav-link no-underline" active-class="active" :to="{ name: 'monitoring-failed', params: { tag:$route.params.tag }}" href="#">
                         Failed Jobs
                     </router-link>
                 </li>
